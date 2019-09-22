cb090a91dd73cec6551eef3198c70690fc513e7a
cbj921 <447933991@qq.com>
2019-09-23 00:09:18 +0800

    完成敌人，其中一个的寻路点完成

---
+++ Assets/Scripts/Pacdot.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pacdot : MonoBehaviour {
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.tag == "Player")
+		{
+			gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Assets/Scripts/ghostMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ghostMove : MonoBehaviour
+{
+    // 储存路径点的transform
+    public Transform[] wayPoints;
+    public float speed = 0.2f;
+    // 路径点的索引
+    private int index = 0;
+    private Rigidbody2D rg2d;
+    private Animator ghostAnimator;
+
+    private void Awake()
+    {
+        rg2d = GetComponent<Rigidbody2D>();
+        ghostAnimator = GetComponent<Animator>();
+    }
+    private void FixedUpdate()
+    {
+        if (transform.position != wayPoints[index].position)
+        {
+            Vector2 temp = Vector2.MoveTowards(transform.position, wayPoints[index].position, speed);
+            rg2d.MovePosition(temp);
+        }
+		else
+		{
+			index = (index + 1) % wayPoints.Length;
+		}
+        Vector2 dirVec = (wayPoints[index].position - transform.position).normalized;
+        ghostAnimator.SetFloat("dirX",dirVec.x);
+        ghostAnimator.SetFloat("dirY",dirVec.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.name == "Pacman")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
